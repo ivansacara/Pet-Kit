0163978ab6c19a6096604f6ea57ecae00e69c465
German Trushkov <germantrushkov@gmail.com>
2023-12-01 20:52:52 +0200

    some changes

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,30 @@
 <template>
-  <header>
-    <h1>Header</h1>
-  </header>
+	<header class="header-main">
+		<div class="header-container">
+			<div class="header-burger">
+				<svg role="presentation" stroke-width="1.4" focusable="false" width="22" height="22" class="icon icon-hamburger" viewBox="0 0 22 22">
+					<path d="M1 5h20M1 11h20M1 17h20" stroke="currentColor" stroke-linecap="round"></path>
+				</svg>
+			</div>
+			<div class="header-logo">
+				<a href="/">
+					<img src="/public/icons/logo.svg" alt="">
+				</a>
+			</div>
+			<div class="header-languages">
+				<ul>
+					<li id="ru">ru</li>
+					<!-- <li id="ro">ro</li> -->
+				</ul>
+			</div>
+		</div>
+	</header>
 </template>
+
 <script setup>
+
 </script>
+
+<style lang="scss">
+	
+</style>
