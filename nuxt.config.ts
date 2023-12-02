940826069caaed3b603e20a0105879ec8caa9a22
German Trushkov <germantrushkov@gmail.com>
2023-12-02 16:34:53 +0200

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/styles/variables/_variables.scss" as *',
+          additionalData: '@use "@/assets/styles/variables/_variables.scss" as *;',
         },
       },
     },
