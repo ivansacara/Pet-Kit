db24aeaa6f9b55ef6acb778560c37bc8e48cc99d
IvanSacara <ivansacara@gmail.com>
2023-12-10 09:12:58 +0200

    test 9

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-<!--    <Product-description :product="product"/>-->
+    <Product-description :product="product"/>
   </div>
 </template>
 <script setup>
-// import {useNuxtApp} from "nuxt/app";
-// const {$client}=useNuxtApp()
-// const route = useRoute();
-// const { t } = useI18n();
-// const product = await $client.getEntries({
-//     content_type: 'product',
-//     "fields.slug": route.params.slug,
-//     locale: t("locale"),
-// })
+import {useNuxtApp} from "nuxt/app";
+const {$client}=useNuxtApp()
+const route = useRoute();
+const { t } = useI18n();
+const product = await $client.getEntries({
+    content_type: 'product',
+    "fields.slug": route.params.slug,
+    locale: t("locale"),
+})
 
 
 </script>
