a067ec1e5f3abd320ce750f73d1a5e2ca40d7308
IvanSacara <ivansacara@gmail.com>
2023-12-27 00:16:02 +0200

    #FIX: TEST_7

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
   <About />
 </template>
 <script setup lang="js">
+import { useRoute, useAsyncData, useHead } from 'nuxt/app';
 const { t } = useI18n();
 const { $client } = useNuxtApp();
 const route = useRoute();
@@ -17,74 +18,72 @@ useHead({
 
 // Fetching categories data
 const categoryType = "category";
-const { data: categoriesData } = await useAsyncData("categories", () =>
+const  data = await useAsyncData("categories", () =>
     $client.getEntries({
         content_type: categoryType,
         'fields.slug': route.params.slug,
         locale: t("locale"),
     })
 );
-console.log(categoriesData)
+console.log(data)
 
 // Processing categories data
-const categoryInformation = computed(() => ({
-    items: categoriesData.value.items.map(item => ({
-        sys: {
-            id: item.sys.id,
-            locale: item.sys.locale.split('-')[0]
-        },
-        fields: {
-            id: item.sys.id,
-            name: item.fields.name,
-            image: item.fields.image?.fields.file?.url,
-            slug: item.fields.slug
-        }
-    })),
-}));
-
-console.log(categoryInformation)
+// const categoryInformation = computed(() => ({
+//     items: categoriesData.value.items.map(item => ({
+//         sys: {
+//             id: item.sys.id,
+//             locale: item.sys.locale.split('-')[0]
+//         },
+//         fields: {
+//             id: item.sys.id,
+//             name: item.fields.name,
+//             image: item.fields.image?.fields.file?.url,
+//             slug: item.fields.slug
+//         }
+//     })),
+// }));
 
 // Fetching sliders data
-const sliderType = "slider";
-const { data: slidersData } = await useAsyncData("sliders", () =>
-    $client.getEntries({
-        content_type: sliderType,
-    })
-);
-
-// Processing sliders data
-const sliderItems = computed(() => {
-    let groupedSlides = slidersData.value.items.flatMap(item =>
-        item.fields.slide.reduce((acc, slideItem) => {
-            let key = slideItem.fields.title; // 'mob' or 'desc'
-            if (!acc[key]) {
-                acc[key] = [];
-            }
-            acc[key].push(slideItem.fields.file.url);
-            return acc;
-        }, {})
-    );
-
-    let slidesArray = [];
-    for (let slideKey in groupedSlides[0]) {
-        let slideNumber = slideKey.split('_')[1]; // Extract slide number
-        let slideType = slideKey.split('_')[2]; // Extract slide type ('mob' or 'desc')
-
-        if (!slidesArray[slideNumber]) {
-            slidesArray[slideNumber] = { mobileUrl: '', desktopUrl: '' };
-        }
-
-        if (slideType === 'mob') {
-            slidesArray[slideNumber].mobileUrl = groupedSlides[0][slideKey][0];
-        } else if (slideType === 'desc') {
-            slidesArray[slideNumber].desktopUrl = groupedSlides[0][slideKey][0];
-        }
-    }
-
-    return {
-        items: slidesArray.filter(slide => slide)
-    };
-});
+// const sliderType = "slider";
+// const { data: slidersData } = await useAsyncData("sliders", () =>
+//     $client.getEntries({
+//         content_type: sliderType,
+//     })
+// );
+//
+// // Processing sliders data
+// const sliderItems = computed(() => {
+//     let groupedSlides = slidersData.value.items.flatMap(item =>
+//         item.fields.slide.reduce((acc, slideItem) => {
+//             let key = slideItem.fields.title; // 'mob' or 'desc'
+//             if (!acc[key]) {
+//                 acc[key] = [];
+//             }
+//             acc[key].push(slideItem.fields.file.url);
+//             return acc;
+//         }, {})
+//     );
+//
+//     let slidesArray = [];
+//     for (let slideKey in groupedSlides[0]) {
+//         let slideNumber = slideKey.split('_')[1]; // Extract slide number
+//         let slideType = slideKey.split('_')[2]; // Extract slide type ('mob' or 'desc')
+//
+//         if (!slidesArray[slideNumber]) {
+//             slidesArray[slideNumber] = { mobileUrl: '', desktopUrl: '' };
+//         }
+//
+//         if (slideType === 'mob') {
+//             slidesArray[slideNumber].mobileUrl = groupedSlides[0][slideKey][0];
+//         } else if (slideType === 'desc') {
+//             slidesArray[slideNumber].desktopUrl = groupedSlides[0][slideKey][0];
+//         }
+//     }
+//
+//     return {
+//         items: slidesArray.filter(slide => slide)
+//     };
+// });
 </script>
 
 
